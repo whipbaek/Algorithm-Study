39fb20292ddf38c500540b492156d24a370d92e3
whipbaek <jibaek0513@gmail.com>
2022-02-15 20:55:34 +0900

    BAEKJOON

--- 분류 - 문자열/10809 알파벳 찾기.cpp
+++ 분류 - 문자열/10809 알파벳 찾기.cpp
@@ -1,20 +1,17 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
 int main(void) {
 
 	vector<int> ans;
-	string str; 
+	string str;
 	cin >> str;
-	char alph = 'a';
 
-	for (int i = 0; i < 26; i++) {
-		if (str.find(alph) != string::npos)
-			ans.push_back(str.find(alph));
+	for (char i = 'a'; i <= 'z'; i++) {
+		if (str.find(i) != string::npos)
+			ans.push_back(str.find(i));
 		else
 			ans.push_back(-1);
-
-		alph++;
 	}
 
 	for (int i = 0; i < ans.size(); i++)
