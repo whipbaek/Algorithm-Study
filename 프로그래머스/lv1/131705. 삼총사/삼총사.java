1342efa170eabeb0981ab17cb63145c28c27c390
whipbaek <75191916+whipbaek@users.noreply.github.com>
2023-08-20 16:27:02 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        for(int i=0; i<number.length; i++){
+            for(int j=i+1; j<number.length; j++){
+                for(int k=j+1; k<number.length; k++){
+                    if(i == k || j == k) continue;
+                    
+                    if(number[i] + number[j] + number[k] == 0) answer++;  
+                    
+                }
+            }
+        }
+        return answer;
+    }
+}
