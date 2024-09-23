101fc2730b611dfc5a804d673f9fab35d69d2142
백종인 <75191916+whipbaek@users.noreply.github.com>
2024-09-23 21:36:36 +0900

    [Gold V] Title: 주사위 쌓기, Time: 452 ms, Memory: 96092 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.java
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.java
@@ -1,20 +1,8 @@
 import java.util.*;
 import java.io.*;
 
-// 1번 주사위는 어떤식으로든 둘 수 있음.
-
-// 이후 주사위들은 아래 주사위의 윗면을 아랫면과 맞추어야 한다.
-
-/**
- * 1번 주사위를 6가지 방법으로 둘 수 있다.
- * 이후 주사위는 모두 그에 맞춰서 배치한다.
- * 주사위를 쌓아올릴때, 아랫면을 기준으로 맞추고, 4개의 옆면중 가장 큰 값을 구한다.
- * 윗면을 아랫면으로 갱신해준다음 주사위가 끝날때까지 반복한다.
- */
-
-
 public class Main {
-
+    public static int[] oppositeValue = {5, 3, 4, 1, 2, 0};
     public void solution() throws Exception {
         int answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -36,35 +24,21 @@ public class Main {
             int tempAnswer = getMaxSide(i, dices.get(0)); // 첫번째 주사위
 
             int floorIndex = i; // 초기 바닥면
-            int floorValue = dices.get(0).get(i);
 
-            int ceilingIndex = getUpSideIndex(i); // 초기 윗면
+            int ceilingIndex = oppositeValue[i]; // 초기 윗면
             int ceilingValue = dices.get(0).get(ceilingIndex);
 
-//            System.out.println("diceNumber : " + i);
-//            System.out.println();
-//            System.out.println("floorIndex : " + floorIndex);
-//            System.out.println("ceilingIndex = " + ceilingIndex);
 
             for (int j = 1; j < dices.size(); j++) { // 첫번째 주사위부터 끝 주사위까지
 
-                // floorNumber 는 ceilingValue 를 가지고 있는 현재 다이스의 인덱스이다.
-                // ceilingIndex 는 floorNumber 의 반대값을 가지고 있어야한다.
-
                 floorIndex = getFloorIndex(ceilingValue, dices.get(j)); // 윗면을 바닥면으로 설정한다.
-                floorValue = dices.get(j).get(floorIndex);
 
-                ceilingIndex = getUpSideIndex(floorIndex); // 윗면을 구한다.
+                ceilingIndex = oppositeValue[floorIndex]; // 윗면을 구한다.
                 ceilingValue = dices.get(j).get(ceilingIndex);
 
-//                System.out.println();
-//                System.out.println("floorIndex : " + floorIndex);
-//                System.out.println("ceilingIndex = " + ceilingIndex);
 
                 tempAnswer += getMaxSide(floorIndex, dices.get(j)); // floorNumber 밑으로 두었을 때 가장 큰 옆면을 구한다.
             }
-//            System.out.println();
-//            System.out.println();
 
             answer = Math.max(answer, tempAnswer);
         }
@@ -89,17 +63,6 @@ public class Main {
         for (int i = 0; i < dice.size(); i++) {
             if(value == dice.get(i)) return i;
         }
-        return -1;
-    }
-
-    public int getUpSideIndex(int floorNumber) {
-        if(floorNumber == 0) return 5;
-        if(floorNumber == 5) return 0;
-        if(floorNumber == 1) return 3;
-        if(floorNumber == 3) return 1;
-        if(floorNumber == 2) return 4;
-        if(floorNumber == 4) return 2;
-
         return -1;
     }
 
