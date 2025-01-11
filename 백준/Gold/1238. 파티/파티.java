0c3694dbb2384ebf13ae0dd455f7f8f3fe323f60
백종인 <75191916+whipbaek@users.noreply.github.com>
2025-01-11 15:41:54 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 23420 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,19 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
-
- N개 마을 (Node?)
- M개 단방향 도로 (edge?)
- X번 마을
-
- 본인의 마을 -> 파티마을
- 파티마을 -> 본인의 마을
-
-
-
- */
-
 public class Main {
 
     static class Node {
@@ -25,7 +12,6 @@ public class Main {
             this.num = num;
             this.weight = weight;
         }
-
     }
 
     public void solution() throws Exception {
@@ -39,6 +25,7 @@ public class Main {
         int x = Integer.parseInt(st.nextToken());
 
         int[][] graph = new int[n+1][n+1];
+        int[][] reverseGraph = new int[n+1][n+1];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
@@ -46,45 +33,24 @@ public class Main {
             int weight = Integer.parseInt(st.nextToken());
 
             graph[from][to] = weight;
+            reverseGraph[to][from] = weight;
         }
 
-        int[] answer = new int[n+1];
 
         // Dijkstra
+        int[] resReverse = dijkstra(x, n, reverseGraph); // 각 정점에서 x 까지의 값
+        int[] res = dijkstra(x, n, graph);  // x에서 각 정점까지의 값
+
+        int maxValue = -1;
         for (int i = 1; i <= n; i++) {
-            if(i == x) continue;
-            int[] temp = dijkstra(i, n, graph);
-            answer[i] = temp[x];
+            maxValue = Math.max(maxValue, res[i] + resReverse[i]);
         }
-
-
-        int[] temp = dijkstra(x, n, graph);
-        for (int i = 1; i <= n; i++) {
-            answer[i] += temp[i];
-        }
-//
-//        for (int i = 1; i <= n; i++) {
-//            System.out.print(answer[i] + " ");
-//        }
-//        System.out.println();
-
-        int idx = -1;
-        int val = -1;
-
-        for (int i = 1; i <= n; i++) {
-            if(answer[i] > val){
-                idx = i;
-                val = answer[i];
-            }
-        }
-
-        System.out.println(val);
+        System.out.println(maxValue);
     }
 
     // startPoint 에서 endPoint 까지의 거리를 구한다.
     public int[] dijkstra(int startPoint, int n, int[][] graph){
-        int res = 0;
-        // 가중치
+
         int[] weights = new int[n+1];
         boolean[] visited = new boolean[n+1];
         for(int i=0; i<n+1; i++) {
@@ -98,7 +64,6 @@ public class Main {
         while (!pq.isEmpty()) {
             Node node = pq.poll();
             int num = node.num;
-            int weight = node.weight;
 
             if(visited[num]) continue;
             visited[num] = true;
@@ -109,24 +74,12 @@ public class Main {
                 if(visited[i]) continue; // 방문한 값이면 패스.
 
                 // 현재 점에서 다른 점으로 이동하는 값이 더 작다면 갱신해준다.
-
                 if(weights[num] + graph[num][i] < weights[i] ){
                     weights[i] =  weights[num] + graph[num][i];
                     pq.add(new Node(i, weights[i]));
                 }
             }
         }
-
-//        System.out.println("========== res ==========");
-//
-//        System.out.println("start[" + startPoint + "] -> end[" + endPoint + "]");
-//        for (int i = 1; i <= n; i++) {
-//            System.out.print(weights[i] + " ");
-//        }
-//        System.out.println();
-//
-//        System.out.println("========== res ==========");
-//        System.out.println();
 
         return weights;
     }
